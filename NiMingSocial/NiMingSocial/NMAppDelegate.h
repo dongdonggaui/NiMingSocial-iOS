a67a5bf8f336c78b50a0b48fcd6fa108ef8ba0b3
hly <huangluyang007@163.com>
2014-05-19 09:20:17 +0800

    初始化

---
+++ NiMingSocial/NiMingSocial/NMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NMAppDelegate.h
+//  NiMingSocial
+//
+//  Created by huangluyang on 14-5-19.
+//  Copyright (c) 2014年 huangluyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NiMingSocial/NiMingSocial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NiMingSocial
+//
+//  Created by huangluyang on 14-5-19.
+//  Copyright (c) 2014年 huangluyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NMAppDelegate class]));
+    }
+}
